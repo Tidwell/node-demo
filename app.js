e339982ef724716f9952c9809cf4f34f028ada4f
Aaron Tidwell <aaron.tidwell@gmail.com>
2014-05-14 12:08:51 -0400

    adding configuration file

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+var config = require('./config');
+
 var express = require('express');
 var routes = require('./routes');
 
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + config.publicDirectory));
 
 routes.bind(app);
 
